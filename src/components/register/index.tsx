c4622c1165ae5d205326454c9c98bfbaa4b43528
Rohit <rohitjain18005@gmail.com>
2020-11-29 19:29:22 +0530

    Added React Camera

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import {RNCamera} from 'react-native-camera';
 import {Text, TouchableOpacity} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useNavigation} from '@react-navigation/native';
+import {PrivateKey} from 'eciesjs';
+import {generatePair} from '../../services/ecc';
+
+interface RegisterScan {
+  hash: string;
+  userId: string;
+}
 
 const Register = () => {
   const navigation = useNavigation();
+
+  const [privateKey, setPrivateKey] = React.useState<string>('');
+  const [publicKey, setPublicKey] = React.useState<string>('');
+
+  React.useEffect(() => {
+    if (privateKey === '' && publicKey === '') {
+      const pKey: PrivateKey = generatePair();
+      setPrivateKey(pKey.toHex());
+      setPublicKey(pKey.toHex());
+    }
+  }, [privateKey, publicKey]);
+
   const onSuccess = (e) => {
-    set(e.data, e.data).then(() => {
+    const hash: RegisterScan = JSON.parse(e.data);
+    console.log(hash);
+    set(hash.userId, privateKey).then(() => {
       navigation.navigate('root');
     });
   };
+
   const set = async (key: string, value: string) => {
     try {
       await AsyncStorage.setItem(key, value);
