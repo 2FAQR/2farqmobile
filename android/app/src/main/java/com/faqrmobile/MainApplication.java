a027101326fe7491759271e421f19d758fb6a205
Rohit <rohitjain18005@gmail.com>
2020-11-29 21:10:27 +0530

    Added nodefy

--- android/app/src/main/java/com/faqrmobile/MainApplication.java
+++ android/app/src/main/java/com/faqrmobile/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.zoontek.rnpermissions.RNPermissionsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
